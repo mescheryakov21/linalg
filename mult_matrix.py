c67e3da0566d5017ba3a901da552690d1bcd1c30
Volody <volody1987@mail.ru>
2022-02-09 16:22:50 +0300

    Practic by Lesson #3 mission #5

--- mult_matrix.py
+++ mult_matrix.py
@@ -1,22 +1,27 @@
 import numpy as np
 
 def multi_matrix (matrix_a, matrix_b,d):
-    k = 0
+    """
+    Функция для произведения матриц
+    Принимает: две матрицы и количество столбцов первой матрицы
+    Возвращает матрица скалярного произведения
+    """
     mult_arr = []
-    etap_1 = []
-    for ii in matrix_a:
-        eff = 0
-        for jj in (matrix_b.T):
-            if k < d:
+    for ii in matrix_a:  # берем строки одной матрицы
+        etap_1 = []
+        for jj in (matrix_b.T):  # берем столбцы второй матрицы, для этого транспонируем
+            eff = 0
+            k = 0
+            while k < d:
                 eff = eff + ii[k]*jj[k]
                 k += 1
-            etap_1.append(eff)
-        mult_arr.append(etap_1)
+            etap_1.append(eff)    # соединение суммы произведений по элементно в вектор
+        mult_arr.append(etap_1)   # соединение векторов в матрицу
     return mult_arr
 
 matrix_a = np.array([[1,2,3],[4,5,6]])
 matrix_b = np.array([[7,8,9],[1,1,1],[2,2,3]])
-a = np.shape(matrix_a)
+a = np.shape(matrix_a)  # определяем размерность матриц для возможности произведения
 b = np.shape(matrix_b)
 if a[1] == b[0]:
     print(multi_matrix(matrix_a, matrix_b, a[1]))
